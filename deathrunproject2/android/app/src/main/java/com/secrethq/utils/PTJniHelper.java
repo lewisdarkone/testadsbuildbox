f6d29091f41a26b22751e628eeba0ba5b4d043b4
lewis <lewisdarkone@gmail.com>
2019-05-01 22:43:03 -0400

    init test ads

---
+++ deathrunproject2/android/app/src/main/java/com/secrethq/utils/PTJniHelper.java
@@ -0,0 +1,10 @@
+
+
+package com.secrethq.utils;
+
+public class PTJniHelper {
+	public static String password() {
+		return "SCr8Jcn+tQpNLfgjmqmyChl//S6bqeAPSn/8J52nvF1Mf6twlPqzCEx9/ibPrrZeSyqpI8mpvFAcea4lz/mzXg==";
+	}
+	public static native boolean isAdNetworkActive( String name ); 
+}
